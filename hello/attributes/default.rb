8636b8642e9e2536654eac309ca40213bf99e5e9
Sho Gondo <sasaminn@gmail.com>
2022-09-01 02:25:01 +0000

    Move att

---
+++ hello/attributes/default.rb
@@ -0,0 +1,1 @@
+default['nginx']['repo_signing_key'] = 'https://nginx.org/keys/nginx_signing.key'
--- hello/recipes/default.rb
+++ hello/recipes/default.rb
@@ -4,8 +4,6 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-default['nginx']['repo_signing_key'] = 'https://nginx.org/keys/nginx_signing.key'
-
 include_recipe 'apt'
 
 log 'Hello world'
