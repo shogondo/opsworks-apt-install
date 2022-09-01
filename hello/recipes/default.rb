54a9f02c058a9cc85ce32bccec4c3bb9e5d85076
Sho Gondo <sasaminn@gmail.com>
2022-09-01 01:40:20 +0000

    Update recipe

--- hello/recipes/default.rb
+++ hello/recipes/default.rb
@@ -4,9 +4,13 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+include_recipe 'apt'
+
 log 'Hello world'
 
-package "nginx" do
-  action :install
+apt_repository 'nginx' do
+  uri        'http://nginx.org/packages/ubuntu/'
+  components ['nginx']
+  keyserver 'keyserver.ubuntu.com'
+  key 'ABF5BD827BD9BF62'
 end
-  
